93eee3f53b9ed96320c41267085cb2e604566cb4
Heather Grant <h.grant@nhm.ac.uk>
2017-07-10 14:42:47 +0100

    now makes multifurcating trees

---
+++ sSTMcmc.py
@@ -0,0 +1,10 @@
+read('tt_mcmc.nex')
+
+stm = STMcmc(var.trees, sampleInterval=10, beta=2.0)
+stm.run(500)
+
+tp = TreePartitions("mcmc_trees_0.nex", skip=25)
+t = tp.consensus(minimumProportion=0.5)
+for n in t.iterInternalsNoRoot():
+    n.name = "%.0f" % (100. * n.br.support)
+t.draw()
