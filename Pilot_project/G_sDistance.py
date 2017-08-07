b9f3b19d5ee950ef8fe244a50e220a94b3b91f33
Heather Grant <h.grant@nhm.ac.uk>
2017-08-07 18:34:51 +0100

    fixed

--- Pilot_project/G_sDistance.py
+++ Pilot_project/G_sDistance.py
@@ -6,19 +6,18 @@ a=func.readAndPop('./results/gene0.phy')
                                                                                                   
 #match master tree and reconstructed tree for calculating their distances                                                                                                                                                                                
 truetree= func.readAndPop('./results/gene.true')                                                  
-truetree.taxNames = a.taxNames                                                                                                                
-
+truetree.name = 'true_tree' 
 mrp = func.readAndPop('mrpStrictConsTree.nex') 
-mrp.taxNames = a.taxNames                                                                                                                
+mrp.name='mrp'
 
 sr2008 = func.readAndPop('SR2008Cons.nex')
-sr2008.taxNames=a.taxNames
+sr2008.name='sr2008'
 
 spa=func.readAndPop('SPA_cons.nex')
-spa.taxNames=a.taxNames
+spa.name='spa'
 
 qpa= func.readAndPop('QPA_cons.nex')
-qpa.taxNames=a.taxNames
+qpa.name='qpa'
 
 var.trees.append(truetree)
 var.trees.append(mrp)
@@ -26,14 +25,16 @@ var.trees.append(sr2008)
 var.trees.append(spa)
 var.trees.append(qpa) 
 mytreelist= [truetree, mrp, sr2008, spa, qpa]
-tt = Trees(mytreelist)
+tt = Trees(mytreelist, taxNames=a.taxNames)
+
 
 dm = tt.topologyDistanceMatrix('sd')
 dm.writeNexus()
+#dm.writeNexus(fName="distancematic.nex")
 
-rf_dist = (truetree.topologyDistance(mrp, metric='sd'))
-print("RF distance from true tree to mrp is %i" % rf_dist)
+#rf_dist = (truetree.topologyDistance(mrp, metric='sd'))
+#print("RF distance from true tree to mrp is %i" % rf_dist)
 
-rf_dist_2 = (spa.topologyDistance(qpa, metric='sd'))
-print("RF distance spa to qpa is %i" %rf_dist_2)
+#rf_dist_2 = (spa.topologyDistance(qpa, metric='sd'))
+#print("RF distance spa to qpa is %i" %rf_dist_2)
 
